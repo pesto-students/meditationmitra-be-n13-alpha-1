1e60cc41976fafad2198b86801337909fb9d380f
Vaaiibhav Vaidya <vaaiibhav@techsparagus.com>
2022-02-16 23:08:43 +0530

    users API complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+// api routes
+
+router.use("/user", userRoutes);
+// router.use("/course", courseRoutes);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Meditation Mitra Backend' });
+  res.send({ title: 'Meditation Mitra Backend' });
 });
 
 module.exports = router;
