14bc4f8b5f98a6554980bdbbf6f11e7e65549467
Vaaiibhav Vaidya <vaaiibhav@techsparagus.com>
2022-02-17 07:50:17 +0530

    courses api complete

--- routes/api/course.js
+++ routes/api/course.js
@@ -1,13 +1,38 @@
 const express = require("express");
 const router = express.Router();
-
+const {auth} = require("../../middleware/auth");
+// import Multer for Image uploads
+const multer = require("multer");
+const upload = multer({ dest: "uploads/images/courses" });
 // Models and Helpers
 const Courses = require("../../models/courses");
 
-router.get("/", async (req, res) => {
+// Get all courses
+router.get("/", auth ,async (req, res) => {
   const courses = await Courses.find();
   res.send(courses);
 });
 
-router.post("/");
+
+// Get a Specific course
+router.get("/:course_query", auth ,async (req, res) => {
+  const {course_query} = req.params;
+  const courses = await Courses.find({
+    name:course_query,
+    category:course_query,
+    createdBy:course_query
+  });
+  res.send(courses);
+});
+// 
+
+
+// add new course
+router.post("/add-course", auth ,upload.file("course-image"),async(req,res)=>{
+const {file,body} = req;
+console.log(req.body);
+console.log(req.files);
+res.json({ message: "Successfully uploaded files" });
+
+});
 module.exports = router;
